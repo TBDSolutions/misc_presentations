78d3090223ee0112792d8b9a477991038e3c865f
Joshh <j-hagedorn@users.noreply.github.com>
2019-06-18 10:08:25 -0400

    Update print_pdf.R

--- print_pdf.R
+++ print_pdf.R
@@ -1,5 +1,6 @@
 # Convert to PDF
 
 library(webshot)
-file_name <- normalizePath("parity_present_20181127.html")
-webshot(file_name, "presentation.pdf")
+file_name <- normalizePath("loc_present_absw_20190626.html")
+webshot(file_name, "loc_present_absw_20190626.pdf")
+
